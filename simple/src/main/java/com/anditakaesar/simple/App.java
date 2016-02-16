84f26ea8c7ad8df950c086c138e175d4717ba3cb
anditakaesar <anditakaesar@gmail.com>
2016-02-16 22:45:02 +0700

    editing App.java to use with CountString class

--- simple/src/main/java/com/anditakaesar/simple/App.java
+++ simple/src/main/java/com/anditakaesar/simple/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StringCount counter = new StringCount();
+		System.out.println(counter.countString("one two three  four"));
     }
 }
